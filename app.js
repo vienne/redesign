09432d20dd05c8deafd56cd29a1ee01c315f1e11
vienne <vienne.chen@gmail.com>
2017-03-28 10:09:09 -0400

    added name color change js and edited about me, adjusted hero images sizes

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-	
+var colors = ['#6CAEB0', '#CF9973', '#FA834F', '#e38e85', '#285687', '#812847', '#B82F2A'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+var firstNameLetter = document.querySelectorAll('.letter');
+
+for (var i= 0; i < firstNameLetter.length; i++) {
+	firstNameLetter[i].style.color = random_color;
+}
